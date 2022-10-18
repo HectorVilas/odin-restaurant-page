940eca00e0f53b243e50fc4cc9be18ab9f91d6db
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2022-10-18 19:17:28 -0300

    add css loader dependencies and rules

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,14 @@ module.exports = {
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+
+  module: {
+    rules: [
+      {test: /\.css$/i, use: ["style-loader", "css-loader"]},
+      {test: /\.(png|svg|jpg|jpeg|gif)/i, use: ["asset/resource"]},
+      {test: /\.(woff|woff2|eot|ttf|otf)/i, type: "asset/resource"},
+    ],
   },
 };
