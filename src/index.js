e43708c44a40c9a0da62b3a408bf7621718ea871
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2022-10-19 18:59:22 -0300

    import menu function and apply on page

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
-import {test,testB} from "./menu";
-import {test2 as t2} from "./toys";
-import {test3} from "./about";
+import {menu} from "./menu";
+// import {test2 as t2} from "./toys";
+// import {test3} from "./about";
 import "./style.css";
 
-console.log(test());
-console.log(testB());
-console.log(t2());
-console.log(test3());
+const body = document.querySelector("#content");
+const content = document.querySelector("#content");
+
+function navBar(){
+  const navBar = document.createElement("div");
+  navBar.classList.add("nav-bar");
+
+  for(let i = 0; i < 2; i++){
+    const goToMenu = document.createElement("div");
+    goToMenu.classList.add("go-to-menu", i === 0 ? "menu-left" : "menu-right");
+    goToMenu.innerText = i === 0 ? "left" : "right";
+    navBar.appendChild(goToMenu);
+  };
+
+  return navBar;
+};
+
+content.appendChild(menu());
+
+body.appendChild(navBar());
