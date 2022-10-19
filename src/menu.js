a031c9e04800373c8c17d22cb3cb6be97637f529
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2022-10-19 19:00:07 -0300

    create 6 items for the food menu

--- src/menu.js
+++ src/menu.js
@@ -1,7 +1,16 @@
-export function test(){
-  return "test menu";
-};
+export function menu(){
+  const menu = document.createElement("div");
+  menu.id = "menu";
+  for(let i = 0; i < 6; i++){
+    const axis = document.createElement("div");
+    axis.classList.add("menu-axis", `menu-axis-${i}`);
 
-export function testB(){
-  return "test B from menu"
-}
+    const item = document.createElement("div");
+    item.classList.add("menu-item", `menu-item-${i}`);
+
+    axis.appendChild(item);
+    menu.appendChild(axis);
+  }
+
+  return menu;
+};
