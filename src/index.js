9dfed48dfd153b0d923cd0463c3b21c24aa87718
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2022-10-20 18:02:16 -0300

    nav buttons actions

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import {menu} from "./menu";
-// import {test2 as t2} from "./toys";
-// import {test3} from "./about";
+import {toys} from "./toys";
+import {about} from "./about";
 
-const body = document.querySelector("#content");
+const menuList = [menu(), toys(), about()];
+let menuIdx = 0;
+
+const body = document.querySelector("body");
 const content = document.querySelector("#content");
 
 function navBar(){
@@ -13,12 +16,21 @@ function navBar(){
     const goToMenu = document.createElement("div");
     goToMenu.classList.add("go-to-menu", i === 0 ? "menu-left" : "menu-right");
     goToMenu.innerText = i === 0 ? "left" : "right";
+    goToMenu.addEventListener("click", menuSwap);
     navBar.appendChild(goToMenu);
+  };
+
+  function menuSwap(){
+    this.className.includes("menu-left") ? menuIdx-- : menuIdx++ ;
+    const i = Math.abs(menuIdx % menuList.length);
+    content.replaceChildren(menuList[i]);
   };
 
   return navBar;
 };
 
-content.appendChild(menu());
+
+
+content.appendChild(menuList[0]);
 
 body.appendChild(navBar());
