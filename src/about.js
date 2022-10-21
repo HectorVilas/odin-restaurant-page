7ff4cf6f29055abcfecdf3dc9b69a13d97be6da6
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2022-10-21 18:54:30 -0300

    add floating hand
    this hand has no use right now, but it will point to
    an adress on a map.

--- src/about.js
+++ src/about.js
@@ -1,7 +1,20 @@
 export function about(){
-  const test = document.createElement("div");
-  test.id = "about"
-  test.innerText = "about";
+  const about = document.createElement("div");
+  about.id = "about";
 
-  return test;
-}
+  const hand = document.createElement("div");
+  const handPartsList = ["hand-palm", "hand-little-finger",
+    "hand-middle-finger", "hand-index", "hand-thumb"];
+  hand.classList.add("hand");
+  for(let i = 0; i < 5; i++){
+    const handPart = document.createElement("img");
+    handPart.classList.add(handPartsList[i]);
+    handPart.src = `./media/images/hands/parts/${handPartsList[i]}.svg`;
+
+    hand.appendChild(handPart);
+  };
+
+  about.appendChild(hand);
+
+  return about;
+};
