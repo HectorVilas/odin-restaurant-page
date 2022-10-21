7fc8c122f86c441fd5cc2481007644f4be48671a
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2022-10-21 16:28:00 -0300

    create signs from code

--- src/index.js
+++ src/index.js
@@ -12,11 +12,22 @@ function navBar(){
   const navBar = document.createElement("div");
   navBar.classList.add("nav-bar");
 
+  
   for(let i = 0; i < 2; i++){
+    const sign = document.createElement("div");
+    sign.classList.add("sign");
+    const post = document.createElement("div");
+    post.classList.add("post");
+    const para = document.createElement("p");
+    para.innerText = i === 0 ? "About" : "Toys";
+    
     const goToMenu = document.createElement("div");
     goToMenu.classList.add("go-to-menu", i === 0 ? "menu-left" : "menu-right");
-    goToMenu.innerText = i === 0 ? "left" : "right";
     goToMenu.addEventListener("click", menuSwap);
+
+    sign.appendChild(para);
+    goToMenu.appendChild(sign);
+    goToMenu.appendChild(post);
     navBar.appendChild(goToMenu);
   };
 
@@ -24,6 +35,10 @@ function navBar(){
     this.className.includes("menu-left") ? menuIdx-- : menuIdx++ ;
     const i = Math.abs(menuIdx % menuList.length);
     content.replaceChildren(menuList[i]);
+    
+    const navParas = document.querySelectorAll(".go-to-menu p");
+    navParas[0].innerText = i == 0 ? "About" : i == 1 ? "Menu" : "Toys";
+    navParas[1].innerText = i == 0 ? "Toys" : i == 1 ? "About" : "Menu";
   };
 
   return navBar;
